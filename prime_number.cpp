28f1958846ae941a87b51c5a8f6e03b0c9e670a2
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-11-09 10:54:41 +0530

    #include<iostream> using namespace std; int main() {     int n,i,c=0;     cin>>n;     for(i=1;i<=n;i++)     {         if (n%i==0)         c++;     }     if (c==2)     cout<<"prime";     else     cout<<"not a prime"; }

---
+++ prime_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,c=0;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if (n%i==0)
+        c++;
+    }
+    if (c==2)
+    cout<<"prime";
+    else
+    cout<<"not a prime";
+}
